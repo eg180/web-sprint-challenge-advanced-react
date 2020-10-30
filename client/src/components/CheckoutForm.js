f7ed8dcb72169ff512ee6b1bca67fef488b284b0
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-30 13:19:29 -0400

    hook working and form working

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from '../hooks/useForm';
 
 const initialValue = {
   firstName: "",
@@ -14,17 +15,12 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const [values, handleChanges, handleSubmit, showSuccessMessage] = useForm(initialValue);
+
 
   return (
     <>
@@ -68,7 +64,6 @@ const CheckoutForm = (props) => {
         </label>
         <button>Checkout</button>
       </form>
-
       {showSuccessMessage && (
         <div className="success-message" data-testid="successMessage">
           <p>
@@ -86,6 +81,7 @@ const CheckoutForm = (props) => {
           </p>
         </div>
       )}
+
     </>
   );
 };
